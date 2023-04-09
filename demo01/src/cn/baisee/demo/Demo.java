cb8255b029a1b6259dcf0963e12660755b27c701
dingyi <349102006@qq.com>
2023-04-09 16:21:06 +0800

    这是我的修改信息

--- demo01/src/cn/baisee/demo/Demo.java
+++ demo01/src/cn/baisee/demo/Demo.java
@@ -3,6 +3,6 @@ package cn.baisee.demo;
 public class Demo {
     public static void main(String[] args) {
         System.out.println(123);
-        System.out.println(1);
+        System.out.println(12);
     }
 }
